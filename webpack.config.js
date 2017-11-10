7926ed7b9068c5e33e55a7faf48ef6335c88f8ba
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-11-11 11:26:08 +1300

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+const src_dirname = path.join(__dirname + '/client/src');
+const dist_dirname = path.join(__dirname + '/dist/');
+
+
+module.exports = {
+  entry: src_dirname + '/index.jsx',
+  output: {
+    path: path.resolve(dist_dirname),
+    filename: 'meowify-bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test : /\.jsx?/,
+        loader : 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
